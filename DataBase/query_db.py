4ff332f224fba196fa31df58daebe855ad41913a
FELIPE BERMOND GONÇALVES <96154703+felipebgoncalves@users.noreply.github.com>
2023-02-13 14:45:35 -0300

    Update query_db.py

--- DataBase/query_db.py
+++ DataBase/query_db.py
@@ -1,11 +1,10 @@
 import streamlit as st
 import DataBase.conection_db as db
 
+
 # FUNÇÃO PARA CONSULTAS NO BANCO DE DADOS
 # Uses st.experimental_memo to only rerun when the query changes or after 10 min.
-st.cache_data(ttl=600)
-
-
+@st.cache_data(ttl=600)
 def consulta_dados(sql_SELECT):
     # CONEXÃO AO DB E CRIAÇÃO DO CURSOR
     cur = db.conexao_db()
