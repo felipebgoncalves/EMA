69cd32f9225447cb4b1fac8c1cd06fedefd7ea9b
FELIPE BERMOND GONÇALVES <96154703+felipebgoncalves@users.noreply.github.com>
2023-02-13 13:39:35 -0300

    Add files via upload

---
+++ DataBase/query_db.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import DataBase.conection_db as db
+
+# FUNÇÃO PARA CONSULTAS NO BANCO DE DADOS
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+st.experimental_memo(ttl=600)
+
+
+def consulta_dados(sql_SELECT):
+    # CONEXÃO AO DB E CRIAÇÃO DO CURSOR
+    cur = db.conexao_db()
+
+    # EXECUÇÃO DO COMANDO SQL
+    cur.execute(sql_SELECT)
+
+    recset = cur.fetchall()
+    registros = []
+
+    for rec in recset:
+        registros.append(rec)
+
+    return registros
