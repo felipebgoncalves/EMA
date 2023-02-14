e2438d022d8cce33b1c88f653fc3b1c293eb8357
FELIPE BERMOND GONÇALVES <96154703+felipebgoncalves@users.noreply.github.com>
2023-02-13 23:37:18 -0300

    Add files via upload

---
+++ Utilities/select_ema.py
@@ -0,0 +1,59 @@
+def seleciona_item(input_ema):
+    if input_ema == 'ADN_01 - Água Doce do Norte':
+        input_ema = 'EMA_ADN_01'
+    if input_ema == 'API_01 - Apiacá':
+        input_ema = 'EMA_API_01'
+    if input_ema == 'ARA_01 - Aracruz':
+        input_ema = 'EMA_ARA_01'
+    if input_ema == 'ARN_01 - Alto Rio Novo':
+        input_ema = 'EMA_ARN_01'
+    if input_ema == 'ATV_01 - Atílio Vivácqua':
+        input_ema = 'EMA_ATN_01'
+    if input_ema == 'CAS_01 - Castelo':
+        input_ema = 'EMA_CAS_01'
+    if input_ema == 'COB_01 - Conceição da Barra':
+        input_ema = 'EMA_COB_01'
+    if input_ema == 'COL_01 - Colatina':
+        input_ema = 'EMA_COL_01'
+    if input_ema == 'GUC_01 - Guaçuí':
+        input_ema = 'EMA_GUC_01'
+    if input_ema == 'ICO_01 - Iconha':
+        input_ema = 'EMA_ICO_01'
+    if input_ema == 'ITG_01 - Itaguaçú':
+        input_ema = 'EMA_ITG_01'
+    if input_ema == 'ITP_01 - Itapemirim':
+        input_ema = 'EMA_ITP_01'
+    if input_ema == 'JAG_01 - Jaguaré':
+        input_ema = 'EMA_JAG_01'
+    if input_ema == 'JON_01 - João Neiva':
+        input_ema = 'EMA_JON_01'
+    if input_ema == 'LAT_01 - Laranja da Terra':
+        input_ema = 'EMA_LAT_01'
+    if input_ema == 'MAF_01 - Marechal Floriano':
+        input_ema = 'EMA_MAF_01'
+    if input_ema == 'MIS_01 - Mimoso do Sul':
+        input_ema = 'EMA_MIS_01'
+    if input_ema == 'PAN_01 - Pancas':
+        input_ema = 'EMA_PAN_01'
+    if input_ema == 'PEC_01 - Pedro Canário':
+        input_ema = 'EMA_PEC_01'
+    if input_ema == 'PIU_01 - Piúma':
+        input_ema = 'EMA_PIU_01'
+    if input_ema == 'RIB_01 - Rio Bananal':
+        input_ema = 'EMA_RIB_01'
+    if input_ema == 'SAL_01 - Santa Leopoldina':
+        input_ema = 'EMA_SAL_01'
+    if input_ema == 'SER_01 - Serra':
+        input_ema = 'EMA_SER_01'
+    if input_ema == 'SRC_01 - São Roque do Canaã':
+        input_ema = 'EMA_SRC_01'
+    if input_ema == 'VAR_01 - Vargem Alta':
+        input_ema = 'EMA_VAR_01'
+    if input_ema == 'VIA_01 - Viana':
+        input_ema = 'EMA_VIA_01'
+    if input_ema == 'VPA_01 - Vila Pavão':
+        input_ema = 'EMA_VPA_01'
+    if input_ema == 'VVA_01 - Vila Valério':
+        input_ema = 'EMA_VVA_01'
+
+    return input_ema
